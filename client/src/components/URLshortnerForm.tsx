e65e9560b9f1a631683376aeff6ec6ed77bdb78f
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-24 15:28:24 +0530

    handled error and favicon chnaged

--- client/src/components/URLshortnerForm.tsx
+++ client/src/components/URLshortnerForm.tsx
@@ -1,4 +1,13 @@
-import { Input, Button, Box, InputGroup } from "@chakra-ui/react";
+import {
+  Input,
+  Button,
+  Box,
+  InputGroup,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+} from "@chakra-ui/react";
+
 import axios from "axios";
 import { useState } from "react";
 import { API_ENDPOINT } from "../config";
@@ -6,16 +15,30 @@ import { API_ENDPOINT } from "../config";
 function URLShortnerForm() {
   const [destination, setDestination] = useState();
   const [shortUrl, setShortUrl] = useState<{ shortId: string } | null>(null);
+  const [error, setError] = useState<string | null>(null);
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setShortUrl(null);
+    setError(null);
 
     const result = await axios
       .post(`${API_ENDPOINT}/api/url`, {
         destination,
       })
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .catch((error: any) => {
+        console.log(error.response.data.message, "erroer");
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.message
+        ) {
+          setError(error.response.data.message);
+        } else {
+          setError("An error occurred");
+        }
+      });
 
     setShortUrl(result);
   }
@@ -27,13 +50,25 @@ function URLShortnerForm() {
             onChange={(e: any) => setDestination(e.target.value)}
             placeholder="https://example.com"
           />
-          <Button type="submit">Create</Button>
+          <Button type="submit" left="2">
+            Create
+          </Button>
         </InputGroup>
       </form>
       {shortUrl && (
-        <a href={`${shortUrl?.shortId}`}>
-          {window.location.origin}/{shortUrl?.shortId}
-        </a>
+        <Button top="2">
+          {shortUrl && (
+            <a href={`${shortUrl?.shortId}`}>
+              {window.location.origin}/{shortUrl?.shortId}
+            </a>
+          )}
+        </Button>
+      )}
+      {error && (
+        <Alert status="error" top="2">
+          <AlertIcon />
+          <AlertDescription>{error}</AlertDescription>
+        </Alert>
       )}
     </Box>
   );
