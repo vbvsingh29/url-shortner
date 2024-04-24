2b37a3de2540bcbddaf2099ff50565a7dc289d4a
vbvsingh29 <vbvsingh2905@gmail.com>
2024-04-24 17:23:56 +0530

    added loading

--- client/src/containers/Home.tsx
+++ client/src/containers/Home.tsx
@@ -1,8 +1,27 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner } from "@chakra-ui/react";
 import Background from "../components/Background";
 import URLShortnerForm from "../components/URLshortnerForm";
+import { useEffect, useState } from "react";
+import { API_ENDPOINT } from "../config";
+import axios from "axios";
 
 function Home() {
+  const [loading, setLoading] = useState<Boolean>(true);
+
+  useEffect(() => {
+    async function healthCheckServer() {
+      return await axios
+        .get(`${API_ENDPOINT}/healthcheck`)
+        .then((res) => {
+          if (res.status === 200) {
+            console.log("object")
+            setLoading(false);
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+    healthCheckServer();
+  });
   return (
     <Box
       height="100%"
@@ -10,7 +29,16 @@ function Home() {
       alignItems="center"
       justifyContent="center"
     >
-      <URLShortnerForm />
+      {loading && (
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      )}
+      {!loading && <URLShortnerForm />}
       <Background />
     </Box>
   );
